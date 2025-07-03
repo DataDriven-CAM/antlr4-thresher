d36207b7c9624ecc7e355f013eafe72e5a51c8d3
rimmartin <waldensianspirit@gmail.com>
2025-07-03 00:43:43 -0400

    inline the u16ncmp function

--- include/parse/u16ncmp.h
+++ include/parse/u16ncmp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace std{
-    size_t u16ncmp(const char16_t* a, const char16_t* b, size_t n){
+    inline size_t u16ncmp(const char16_t* a, const char16_t* b, size_t n){
         for(size_t i=0;i<n;i++){
             if(a[i]>b[i])return 1;
             else if(a[i]<b[i])return -1;
